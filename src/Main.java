ec4ae8f129c0912972c6e282a45d0fc0e63563f9
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-07-19 01:02:06 +0800

    119. Parameterized Statement

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,40 @@
+import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class Main {
+    private static Connection c;
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        String sql_statement = "select * from Javatab";
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaDB", "pluto", "lintonwin");
+        initializeDB();
+
+        String name = JOptionPane.showInputDialog("Fined a javatab by name.");
+        String sql_statement = "select * from javatab where javatabName = ? ; ";
+        PreparedStatement pps = c.prepareStatement(sql_statement);
+        pps.setString(1, name);
+        ResultSet rs = pps.executeQuery();
+        if(rs.next()){
+            int title = Integer.parseInt(rs.getString("javatabId"));
+            String jname = rs.getString("javatabName");
+            int price = Integer.parseInt(rs.getString("price"));
+            JOptionPane.showMessageDialog(null, new Javatab(title, jname,price));
+        }else{
+            JOptionPane.showMessageDialog(null, "not found...");
+        }
+
+        closeDB();
+    }
+
+    private static void initializeDB() throws SQLException {
+        c = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaDB", "pluto", "lintonwin");
         if (c != null) {
             System.out.println("Connecting to the database");
         } else {
             System.out.println("Cannot connect to the database");
         }
 
-        PreparedStatement pps = c.prepareStatement(sql_statement);
-        ResultSet rs = pps.executeQuery();
+    }
 
-        ArrayList<Javatab> result = new ArrayList<>();
-        while (rs.next()){
-            Javatab v = new Javatab(Integer.parseInt(rs.getString("javatabId")),rs.getString("javatabName"),Integer.parseInt(rs.getString("price")));
-            result.add(v);
-        }
-
-        for (Javatab v : result){
-            System.out.println(v.toString());
-        }
-
+    private static void closeDB() throws SQLException{
         c.close();
     }
 }
